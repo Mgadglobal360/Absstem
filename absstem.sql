7bc27add50636b6b1201c95583d78eed24240762
Mgadglobal360 <manish.gupta@adglobal360.com>
2022-07-29 16:02:17 +0530

    'database'

---
+++ absstem.sql
@@ -0,0 +1,71 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.33 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for absstem
+DROP DATABASE IF EXISTS `absstem`;
+CREATE DATABASE IF NOT EXISTS `absstem` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `absstem`;
+
+-- Dumping structure for table absstem.ci_countries
+DROP TABLE IF EXISTS `ci_countries`;
+CREATE TABLE IF NOT EXISTS `ci_countries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sortname` varchar(3) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `phonecode` int(11) NOT NULL,
+  `rule_or_pattern` varchar(200) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `currency_id` int(10) unsigned DEFAULT '0',
+  `timezone_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=248 DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table absstem.contact
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `intersted_in` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table absstem.enquiry
+DROP TABLE IF EXISTS `enquiry`;
+CREATE TABLE IF NOT EXISTS `enquiry` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
